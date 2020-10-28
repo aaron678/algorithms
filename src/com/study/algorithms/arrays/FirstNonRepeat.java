df9c027522398ccfe9e5d4df0ca6c1afe67f1888
aaron678 <serf90@hotmail.com>
2020-10-27 21:47:47 -0400

    minor refactoring

--- src/com/study/algorithms/arrays/FirstNonRepeat.java
+++ src/com/study/algorithms/arrays/FirstNonRepeat.java
@@ -7,11 +7,11 @@ import java.util.Set;
 public class FirstNonRepeat  {
 	
 	Character firstNotRepeatingCharcacter(String s) {
-		Set<Character> firstNonRepeat = new LinkedHashSet<Character>();
-		Set<Character> iWuzAlreadyRemoved = new HashSet<Character>();
+		Set<Character> firstNonRepeat = new LinkedHashSet<>();
+		Set<Character> iWuzAlreadyRemoved = new HashSet<>();
 		
 		for (int i =0; i<s.length(); i++ ){
-			Character c  = new Character(s.charAt(i));
+			Character c  = Character.valueOf(s.charAt(i));
 			if(firstNonRepeat.contains(c)){
 				firstNonRepeat.remove(c);
 				iWuzAlreadyRemoved.add(c);
